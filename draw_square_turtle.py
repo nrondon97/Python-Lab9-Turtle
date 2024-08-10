e75feb76734b6434fc287bee2d59b2886ca609bb
nrondon97 <150634151+nrondon97@users.noreply.github.com>
2024-08-09 21:46:46 -0400

    Add files via upload

---
+++ draw_square_turtle.py
@@ -0,0 +1,12 @@
+# draw_square_turtle.py
+# Naylene Rondon
+# 4/5/17
+# program
+
+from ds import *
+import turtle
+
+t = turtle.Turtle()
+drawSquare(t,150)
+#drawSquare(t,250)
+#drawSquare(t,350)
---
+++ ds.py
@@ -0,0 +1,9 @@
+# ds.py
+# Naylene Rondon
+# 4/5/17
+# arguement in turtle
+
+def drawSquare(myTurtle, sidelength):
+    for i in range(4):
+        myTurtle.forward(sidelength)
+        myTurtle.right(90)
